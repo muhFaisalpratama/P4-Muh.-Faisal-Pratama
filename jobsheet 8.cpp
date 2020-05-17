2bf4a260de02ebf7b74fe79a8dd11f9642d8fa53
muhFaisalpratama <65480592+muhFaisalpratama@users.noreply.github.com>
2020-05-17 13:14:44 +0700

    Create jobsheet 8.cpp

---
+++ jobsheet 8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x[3],i;
+cout<<"masukkan NIM terakhir Anda"<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"Masukkan nim "<<i+1<<" : ";
+cin>>x[i];
+}
+cout<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"nim ke- "<<i+1<<" = "<<x[i]<<endl;
+} cout<<endl;
+
+return 0;
+}
