58015b113dcde1c6e8fcd1c3933aec1215a8d2a3
muhFaisalpratama <65480592+muhFaisalpratama@users.noreply.github.com>
2020-05-17 12:51:34 +0700

    Create jobsheet 1 .cpp
    Menampilkan array

---
+++ jobsheet 1 .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama : Muh. faisal pratama\n";
+	cout<<"Nim : F1B019092\n";
+	cout<<"Kelompok: Kelompok 19\n\n";
+int nim[12]= {1,7,18,13,48,18,20,69,40,50,110,};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+    cout << "Nim ke-6 adalah " << nim[5] << endl;
+    cout << "Nim ke-7 adalah " << nim[6] << endl;
+    cout << "Nim ke-8 adalah " << nim[7] << endl;
+    cout << "Nim ke-9 adalah " << nim[8] << endl;
+    cout << "Nim ke-10 adalah " << nim[9] << endl;
+    cout << "Nim ke-11 adalah " << nim[10] << endl;
+    cout << "Nim ke-12 adalah " << nim[11] << endl;
+   
+}
